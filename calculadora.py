491c1937ef2fb7045584c6b940209502996515a7
Aless SL <106497836+AlessProgramOFFICIAL@users.noreply.github.com>
2022-05-29 19:48:05 -0500

    Update calculadora.py

--- calculadora.py
+++ calculadora.py
@@ -1,9 +1,13 @@
 #Calculadora Diseñada
+#Perla Es Mi Vida
 
+#Se Denominaran Valores A Insertar
 numero1 = input("Inserta un Numero: ")
 numero2 = input("Inserta el segundo Numero: ")
 
+#En Está Parte Se Ejecutará la Suma
 suma = float(numero1) + float(numero2)
 
-print("El Resultado es:" suma)
+#En Está Parte Se Proyectará El Resultado
+print("El Resultado es:", suma)
 print("Está Calculadora Fue Desarrollada el 29/05/22")
